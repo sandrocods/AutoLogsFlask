c5b69e3ab17c38ac6b883193d3b2a10142bc8de0
sandrocods <59155826+sandrocods@users.noreply.github.com>
2022-07-09 10:11:22 +0700

    update v1.1

--- main.py
+++ main.py
@@ -20,19 +20,25 @@ def hello():
 
 #
 # The function starts by emitting a message to the client to disable the button.
-# Then it loops through 50 times, emitting a message to the client with the progress.
+# Then it loops through 100 times, emitting a message to the client with the progress.
 # Finally, it emits a message to the client to enable the button
 # param message: The message sent by the client
 #
 
 @socketio.on('start')
 def start(message):
+    # disable button
     emit('button_action', {'data': 'disable'})
-    for i in range(50):
+
+    # Logs data to the console
+    for i in range(100):
         emit('my response',
              {'data': '[ ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] Progress ' + str(i) + '%'})
         socketio.sleep(0.10)
+
     emit('my response', {'data': '[ ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] Progress Done'})
+
+    # enable button
     emit('button_action', {'data': 'enable'})
 
 
@@ -41,6 +47,7 @@ def start(message):
 #
 @socketio.on('connect')
 def connect():
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' Client connected')
     emit('my response', {'data': '[ ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '] Status : Connected'})
 
 
@@ -49,7 +56,7 @@ def connect():
 #
 @socketio.on('disconnect')
 def test_disconnect():
-    print('Client disconnected')
+    print(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' Client disconnected')
 
 
 if __name__ == '__main__':
